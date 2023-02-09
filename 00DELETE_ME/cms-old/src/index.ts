b837c81de46af026b8cd33ba75f985ef07c8c1fd
Mo Sattler <hi@mosattler.com>
2023-02-09 16:49:02 +0100

    more cleanup

--- 00DELETE_ME/cms-old/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from 'payload';
-export { default as payload } from 'payload';
-export { default } from './config';
-export * from './types';
-export { usersSlug } from './collections/Users';
-export { mediaSlug } from './collections/Media';
-export { pagesSlug } from './collections/Pages';
