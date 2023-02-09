78c80b5f3727ddda367e4c1014a89b39b1a08a9d
Mo Sattler <hi@mosattler.com>
2023-02-09 16:33:04 +0100

    more stuff

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -3,107 +3,88 @@ import fs = require('fs');
 
 import express = require('express');
 import cms = require('@teamlearns/cms');
-import shared = require('@org/shared');
 import webExpressAdapter = require('@teamlearns/web/express');
+import compression from 'compression';
+import morgan from 'morgan';
 
 const { payload } = cms;
-const { dotenv } = shared;
 const { createRequestHandler } = webExpressAdapter;
 
-// Loading environment variables, .env > .env.local
-const config = dotenv.config();
-
-if (config.error) {
-    throw config.error;
-}
-
-const localEnvFilePath = path.resolve(process.cwd(), '.env.local');
-if (fs.existsSync(localEnvFilePath)) {
-    const localConfig = dotenv.config({
-        path: localEnvFilePath,
-        override: true,
-    });
-    if (localConfig.error) {
-        throw localConfig.error;
-    }
-}
-
-const MONGODB_URL = process.env.MONGODB_URL;
-const PAYLOADCMS_SECRET = process.env.PAYLOADCMS_SECRET;
-const ENVIRONMENT = process.env.NODE_ENV;
-
 // During development this is fine. Conditionalize this for production as needed.
-const WEB_BUILD_DIR = path.join(process.cwd(), '../web/build');
-const WEB_PUBLIC_DIR = path.join(process.cwd(), '../web/public/web');
-const WEB_PUBLIC_BUILD_DIR = path.join(
-    process.cwd(),
-    '../web/public/web/build'
-);
+const BUILD_DIR = path.join(process.cwd(), '../web/build');
+const PUBLIC_DIR = path.join(process.cwd(), '../web/public');
+const PUBLIC_BUILD_DIR = path.join(process.cwd(), '../web/public/build');
 
-const app = express();
-app.disable('x-powered-by');
+const start = async () => {
+    const app = express();
 
-// Serving the web static files with different caching strategies
-app.use(
-    '/web/build',
-    express.static(WEB_PUBLIC_BUILD_DIR, {
-        immutable: true,
-        maxAge: '1y',
-        redirect: false,
-    })
-);
-app.use(
-    '/web',
-    express.static(WEB_PUBLIC_DIR, { maxAge: '1h', redirect: false })
-);
+    await payload.init({
+        mongoURL: 'mongodb://localhost/remix-server',
+        secret: 'asdasdasd',
+        express: app,
+        onInit: () => {
+            payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
+        },
+    });
 
-payload.init({
-    express: app,
-    mongoURL: MONGODB_URL,
-    secret: PAYLOADCMS_SECRET,
-    onInit: () => {
-        payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
-    },
-});
+    app.use(payload.authenticate);
 
-// app.use(payload.authenticate);
+    app.use(compression());
 
-app.all(
-    '*',
-    ENVIRONMENT === 'development'
-        ? (req, res, next) => {
-              purgeRequireCache();
+    // http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
+    app.disable('x-powered-by');
 
-              return createRequestHandler({
-                  build: require(WEB_BUILD_DIR),
-                  mode: ENVIRONMENT,
+    // Remix fingerprints its assets so we can cache forever.
+    app.use(
+        '/build',
+        express.static(PUBLIC_BUILD_DIR, { immutable: true, maxAge: '1y' })
+    );
+
+    // Everything else (like favicon.ico) is cached for an hour. You may want to be
+    // more aggressive with this caching.
+    app.use(express.static(PUBLIC_DIR, { maxAge: '1h' }));
+
+    app.use(morgan('tiny'));
+
+    app.all(
+        '*',
+        process.env.NODE_ENV === 'development'
+            ? (req, res, next) => {
+                  purgeRequireCache();
+
+                  return createRequestHandler({
+                      build: require(BUILD_DIR),
+                      mode: process.env.NODE_ENV,
+                      getLoadContext(req, res) {
+                          return {
+                              payload: req.payload,
+                              user: req.user,
+                              res: res,
+                          };
+                      },
+                  })(req, res, next);
+              }
+            : createRequestHandler({
+                  build: require(BUILD_DIR),
+                  mode: process.env.NODE_ENV,
                   getLoadContext(req, res) {
                       return {
                           payload: req.payload,
-                          user: req?.user,
-                          res,
+                          user: req.user,
+                          res: res,
                       };
                   },
-              })(req, res, next);
-          }
-        : createRequestHandler({
-              build: require(WEB_BUILD_DIR),
-              mode: ENVIRONMENT,
-              getLoadContext(req, res) {
-                  return {
-                      payload: req.payload,
-                      user: req?.user,
-                      res,
-                  };
-              },
-          })
-);
+              })
+    );
 
-const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Express server listening on port ${port}`);
-});
+    app.listen(port, () => {
+        console.log(`Express server listening on port ${port}`);
+    });
+};
+
+start();
 
 function purgeRequireCache() {
     // purge require cache on requests for "server side HMR" this won't let
@@ -111,9 +92,8 @@ function purgeRequireCache() {
     // alternatively you can set up nodemon/pm2-dev to restart the server on
     // file changes, but then you'll have to reconnect to databases/etc on each
     // change. We prefer the DX of this, so we've included it for you by default
-
     for (const key in require.cache) {
-        if (key.startsWith(WEB_BUILD_DIR)) {
+        if (key.startsWith(BUILD_DIR)) {
             delete require.cache[key];
         }
     }
